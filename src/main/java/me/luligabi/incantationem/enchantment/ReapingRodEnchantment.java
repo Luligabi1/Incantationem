acd7902613e308cca6e27baa5e53a45cf3b233ad
Luligabi1 <luligabi111@gmail.com>
2021-05-08 13:21:37 -0300

    Nerfed some reaping rod aspects

--- src/main/java/me/luligabi/incantationem/enchantment/ReapingRodEnchantment.java
+++ src/main/java/me/luligabi/incantationem/enchantment/ReapingRodEnchantment.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 
@@ -28,13 +29,12 @@ public class ReapingRodEnchantment extends Enchantment {
 
     @Override
     public void onTargetDamaged(LivingEntity user, Entity target, int level) {
-        if(target instanceof LivingEntity) {
+        if(target instanceof HostileEntity) {
             if(((LivingEntity) target).getHealth() <= 0.5) {
-                Util.applyEffectIfNotPresent(user, StatusEffects.REGENERATION, 20, 1);
-                Util.applyEffectIfNotPresent(user, StatusEffects.ABSORPTION, 90, 3);
-                Util.applyEffectIfNotPresent(user, StatusEffects.RESISTANCE, 5*60, 0);
-                Util.applyEffectIfNotPresent(user, StatusEffects.FIRE_RESISTANCE, 5*60, 0);
-                Util.sendActionBarMessage(user, new TranslatableText("message.incantationem.retreat.applied"), Formatting.LIGHT_PURPLE);
+                Util.applyEffectIfNotPresent(user, StatusEffects.REGENERATION, 10, 1);
+                Util.applyEffectIfNotPresent(user, StatusEffects.ABSORPTION, 25, 2);
+                Util.applyEffectIfNotPresent(user, StatusEffects.RESISTANCE, 30, 0);
+                Util.sendActionBarMessage(user, new TranslatableText("message.incantationem.reaping_rod.applied"), Formatting.LIGHT_PURPLE);
             }
         }
         super.onTargetDamaged(user, target, level);
