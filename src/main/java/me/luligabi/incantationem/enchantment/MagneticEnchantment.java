151fd1de11cdf8af370fb8bd973a0bcdd837e999
Luligabi1 <luligabi111@gmail.com>
2021-05-27 19:44:44 -0300

    Fixed Magnetic Enchantment, changed to be leggings-only.

--- src/main/java/me/luligabi/incantationem/enchantment/MagneticEnchantment.java
+++ src/main/java/me/luligabi/incantationem/enchantment/MagneticEnchantment.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class MagneticEnchantment extends Enchantment {
 
     public MagneticEnchantment() {
-        super(Rarity.VERY_RARE, EnchantmentTarget.ARMOR, new EquipmentSlot[]{EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET});
+        super(Rarity.VERY_RARE, EnchantmentTarget.ARMOR_LEGS, new EquipmentSlot[]{EquipmentSlot.LEGS});
     }
 
     public int getMinPower(int level) {
@@ -33,11 +33,11 @@ public class MagneticEnchantment extends Enchantment {
         BlockPos box1 = new BlockPos(pos.x + (level*2), pos.y + (level*2), pos.z + (level*2));
         BlockPos box2 = new BlockPos(pos.x - (level*2), pos.y - (level*2), pos.z - (level*2));
 
-        List<Entity> entities = world.getEntitiesByType(null, new Box(box1, box2), e -> e instanceof ItemEntity || e instanceof ExperienceOrbEntity);
+        List<Entity> entities = world.getOtherEntities(null, new Box(box1, box2), e -> e instanceof ItemEntity || e instanceof ExperienceOrbEntity);
 
-         /*for(Entity e : entities) { //TODO: Fix reverseSubtract
-            Vec3d velocity = e.getPos().reverseSubtract(new Vec3d(pos.x + 0.5, pos.y + 0.5, pos.z + 0.5)).normalize().multiply(0.1);
+         for(Entity e : entities) {
+            Vec3d velocity = e.getPos().relativize(new Vec3d(pos.x + 0.5, pos.y + 0.5, pos.z + 0.5)).normalize().multiply(0.1);
             e.addVelocity(velocity.x, velocity.y, velocity.z);
-        } */
+        }
     }
 }
