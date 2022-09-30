7be29b976d271b73baf173b9468b9393989f99f7
Luligabi1 <33578169+Luligabi1@users.noreply.github.com>
2022-09-30 16:18:57 -0300

    Fix #16

--- src/main/java/me/luligabi/incantationem/mixin/BlockMixin.java
+++ src/main/java/me/luligabi/incantationem/mixin/BlockMixin.java
@@ -1,5 +1,6 @@
 package me.luligabi.incantationem.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import me.luligabi.incantationem.Util;
 import me.luligabi.incantationem.curse.CurseRegistry;
 import me.luligabi.incantationem.enchantment.EnchantmentRegistry;
@@ -19,47 +20,43 @@ import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Random;
 
 @Mixin(Block.class)
 public abstract class BlockMixin {
 
-    // Mixin for Recklessness Curse.
-    @Inject(method = "afterBreak",
-            at = @At("HEAD"),
-            cancellable = true)
-    public void afterBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack stack, CallbackInfo callbackInfo) {
+    // Recklessness Curse
+    @Inject(
+            method = "afterBreak",
+            at = @At("HEAD")
+    )
+    public void afterBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, BlockEntity blockEntity, ItemStack stack, CallbackInfo ci) {
         player.incrementStat(Stats.MINED.getOrCreateStat(((Block) (Object) this)));
         player.addExhaustion(0.005F);
         if(EnchantmentHelper.getEquipmentLevel(CurseRegistry.RECKLESSNESS, player) < 1) {
             Block.dropStacks(state, world, pos, blockEntity, player, stack);
         }
-        callbackInfo.cancel();
     }
 
-    //Mixin for Forging Touch Enchantment.
-    @Inject(method = "getDroppedStacks(Lnet/minecraft/block/BlockState;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/entity/Entity;Lnet/minecraft/item/ItemStack;)Ljava/util/List;",
-            at = @At("RETURN"),
-            cancellable = true)
-    private static void getDroppedStacks(BlockState state, ServerWorld world, BlockPos pos, BlockEntity blockEntity, Entity entity, ItemStack stack, CallbackInfoReturnable<List<ItemStack>> callbackInfo) {
+    // Forging Touch
+    @ModifyReturnValue(
+            method = "getDroppedStacks(Lnet/minecraft/block/BlockState;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/entity/Entity;Lnet/minecraft/item/ItemStack;)Ljava/util/List;",
+            at = @At("RETURN")
+    )
+    private static List<ItemStack> getDroppedStacks(List<ItemStack> original, BlockState state, ServerWorld world, BlockPos pos, BlockEntity blockEntity, Entity entity, ItemStack stack) {
         List<ItemStack> itemsToDropList = new ArrayList<>();
 
         int forgingTouchLevel = EnchantmentHelper.getLevel(EnchantmentRegistry.FORGING_TOUCH, stack);
-        if (forgingTouchLevel == 0) {
-            callbackInfo.setReturnValue(callbackInfo.getReturnValue());
-            return;
-        }
-        for (ItemStack preForgingItems : callbackInfo.getReturnValue()) {
+        if (forgingTouchLevel <= 0) return original;
+
+        for (ItemStack preForgingItems : original) {
             Optional<SmeltingRecipe> recipe = world.getRecipeManager().listAllOfType(RecipeType.SMELTING).stream().filter((
                     smeltingRecipe -> smeltingRecipe.getIngredients().get(0).test(preForgingItems))).findFirst();
 
@@ -75,6 +72,6 @@ public abstract class BlockMixin {
             }
 
         }
-        callbackInfo.setReturnValue(itemsToDropList);
+        return itemsToDropList;
     }
 }
